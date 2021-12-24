dc1fdc5a7eaa36df70310aa37097f10afd903678
MaragkoudakisEmmanuel <96624509+MaragkoudakisEmmanuel@users.noreply.github.com>
2021-12-24 22:20:03 +0000

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,17 +4,18 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(0)
-    pins.servoWritePin(AnalogPin.P0, 0)
+    pins.servoWritePin(AnalogPin.P0, 1)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(180)
-    pins.servoWritePin(AnalogPin.P0, 180)
+    pins.servoWritePin(AnalogPin.P0, 179)
 })
+let ServoPort = 0
 basic.showNumber(0)
-pins.servoWritePin(AnalogPin.P0, 0)
+pins.servoWritePin(AnalogPin.P0, 1)
 basic.pause(2000)
 basic.showNumber(180)
-pins.servoWritePin(AnalogPin.P0, 180)
+pins.servoWritePin(AnalogPin.P0, 179)
 basic.pause(2000)
 basic.showNumber(90)
 pins.servoWritePin(AnalogPin.P0, 90)
