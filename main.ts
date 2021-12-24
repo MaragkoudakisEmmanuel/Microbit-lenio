5901b322d625ee5d9cb948d56aa825ab0690a04d
MaragkoudakisEmmanuel <96624509+MaragkoudakisEmmanuel@users.noreply.github.com>
2021-12-24 21:43:50 +0000

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(90)
+    pins.servoWritePin(AnalogPin.P0, 90)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0)
+    pins.servoWritePin(AnalogPin.P0, 0)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(180)
+    pins.servoWritePin(AnalogPin.P0, 180)
+})
+basic.showNumber(0)
+pins.servoWritePin(AnalogPin.P0, 0)
+basic.pause(2000)
+basic.showNumber(180)
+pins.servoWritePin(AnalogPin.P0, 180)
+basic.pause(2000)
+basic.showNumber(90)
+pins.servoWritePin(AnalogPin.P0, 90)
 basic.forever(function () {
 	
 })
